67c7964f301da515b792f297f78200a5ec4db212
Katarina Siklodyova <hrobkova@hotmail.com>
2024-02-04 10:58:27 +0000

    first programs

--- hello2.py
+++ hello2.py
@@ -1,16 +1,9 @@
-name = input ("Katarina Siklodyova")
 name = "Katarina"
-print ('Hello \a' + name + '\a nice to meet you')
-
-name  = "Katarina"
-print ('Hello \a' + name)
+print ('Hello ' + name + ' nice to meet you!')
 
 age = 45
 print ('Your age is ' + str(age))
-print ('Hello {}\a your age is {}' .format(name,age))
+print ('Hello {}  your age is {}' .format(name,age))
 
-name = "Mama"
-input("what is your name?")
-print(f"Ahoj {name}")
-
-print ('hello my name is \a"Katka" \aja som tvoja \amamka')
+name = input("what is your name?  ")
+print(f"Hello {name}")
